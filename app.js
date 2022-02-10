f21640697ddcb6b8d46bd9746cdba965ca6fdfcd
Gladys <gladys.ni@yahoo.fr>
2022-02-10 11:55:13 +0100

    Fixing server issues

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use("/api/*", (req, res, next) => {
 if (process.env.NODE_ENV === "production") {
   app.use("*", (req, res, next) => {
     // If no routes match, send them the React HTML.
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
   });
 }
 
